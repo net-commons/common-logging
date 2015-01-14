987b3dcbad486728ec8c4473428fdeec65331d8d
FroggieFrog <froggie1986@gmx.de>
2015-01-14 12:04:17 +0100

    missed a version value

--- src/Common.Logging.NLog32/AssemblyInfo.cs
+++ src/Common.Logging.NLog32/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Security;
 
-[assembly: AssemblyProduct("Common Logging Framework NLog 3.1 Adapter")]
+[assembly: AssemblyProduct("Common Logging Framework NLog 3.2 Adapter")]
 [assembly: SecurityTransparent]
 
 #if NET_4_0
