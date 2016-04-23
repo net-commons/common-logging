6679472a34282c4818721aebd49fef03901d6c95
Steve Bohlen <sbohlen@gmail.com>
2016-04-23 13:16:32 -0400

    initial failing test for proper read of config file

--- test/Common.Logging.MultipleLogger.Tests.2010-net40/Logging/MultipleLogger/FileBasedConfigurationTests.cs
+++ test/Common.Logging.MultipleLogger.Tests.2010-net40/Logging/MultipleLogger/FileBasedConfigurationTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Common.Logging.Log4Net;
+using Common.Logging.NLog;
+using NUnit.Framework;
 
 namespace Common.Logging.MultipleLogger.Tests.Logging.MultipleLogger
 {
@@ -6,11 +11,18 @@ namespace Common.Logging.MultipleLogger.Tests.Logging.MultipleLogger
     public class FileBasedConfigurationTests
     {
         [Test]
-        public void MyMethod()
+        public void CanConfigureMultipleLoggersFromFile()
         {
             var multiLogger = LogManager.GetLogger<FileBasedConfigurationTests>();
 
+            //the only way to test for certain is to use private reflection to grab the configured loggers...
+            var configuredLoggers =
+                typeof (MultiLogger).GetField("_loggers", BindingFlags.NonPublic | BindingFlags.Instance)?
+                    .GetValue(multiLogger) as List<ILog>;
 
+            Assert.That(configuredLoggers, Is.Not.Null);
+            Assert.That(configuredLoggers.Any(logger => logger is Log4NetLogger));
+            Assert.That(configuredLoggers.Any(logger => logger is NLogLogger));
         }
     }
 }
