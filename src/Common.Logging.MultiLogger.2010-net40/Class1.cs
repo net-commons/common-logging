959d8ee8a7ebce7504da0fcd31c76cdf4c1422bf
Steve Bohlen <sbohlen@gmail.com>
2016-04-23 08:57:06 -0400

    inital add of new src project

---
+++ src/Common.Logging.MultiLogger.2010-net40/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Logging.MultiLogger._2010_net40
+{
+    public class Class1
+    {
+    }
+}
