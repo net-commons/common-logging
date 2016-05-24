26725ed1a3d5e6318dff9e4a9f96dfd6f132b01a
Steve Bohlen <sbohlen@gmail.com>
2016-05-24 15:43:07 -0400

    create ETWLogger class

--- src/Common.Logging.ETWLogger/Logging/ETW/ETWLogger.cs
+++ src/Common.Logging.ETWLogger/Logging/ETW/ETWLogger.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Common.Logging.ETWLogger
+namespace Common.Logging.ETW
 {
-    public class Class1
+    public class ETWLogger
     {
     }
 }
