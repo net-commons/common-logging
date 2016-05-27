624f86d511e6e63b4613cd63f92e71a8496b3e93
Steve Bohlen <sbohlen@gmail.com>
2016-05-27 17:31:38 -0400

    refactoring tests

---
+++ test/Common.Logging.ETWLogger.Tests/Logging/ETWLogger/BasicIntegrationTestHarness.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Logging.Configuration;
+using Common.Logging.ETW;
+using NUnit.Framework;
+
+namespace Common.Logging.ETWLogger.Tests
+{
+    [TestFixture]
+    public class BasicIntegrationTestHarness
+    {
+        [Test]
+        public void BasicLoggingScenario()
+        {
+            var adapter = new ETWLoggerFactoryAdapter();
+            var logger = adapter.GetLogger(string.Empty);
+
+            logger.Warn("This is a test message from ETW source!");
+        }
+
+        [Test]
+        public void LoggingWithCustomEventSource()
+        {
+            var adapter = new ETWLoggerFactoryAdapter { ETWEventSource = new MyCustomEventSource() };
+            var logger = adapter.GetLogger(string.Empty);
+
+            logger.Warn("This message should never appear in the ETW logs b/c its ignored by the custom EventSource for the sake of the test!");
+        }
+
+        [Test]
+        public void LoggingWithException()
+        {
+            var adapter = new ETWLoggerFactoryAdapter();
+            var logger = adapter.GetLogger(string.Empty);
+
+            logger.Debug("This is a test message from ETW source!", new Exception("I am the test exception"));
+        }
+    }
+}
---
+++ test/Common.Logging.ETWLogger.Tests/Logging/ETWLogger/ETWLoggerTests.cs
@@ -0,0 +1,23 @@
+﻿using Common.Logging.Configuration;
+using Common.Logging.ETW;
+using NUnit.Framework;
+
+namespace Common.Logging.ETWLogger.Tests
+{
+    [TestFixture]
+    public class ETWLoggerTests
+    {
+        [Test]
+        public void ExplicitLoggingLevelImplicitlyIncludesGreaterLoggingLevels()
+        {
+            Assume.That(LogLevel.Fatal > LogLevel.Debug, "Relationship between FATAL and DEBUG logging levels not as expected.");
+
+            var props = new NameValueCollection { { "level", "debug" } };
+            var adapter = new ETWLoggerFactoryAdapter(props);
+
+            var logger = adapter.GetLogger(string.Empty);
+
+            Assert.That(logger.IsFatalEnabled);
+        }
+    }
+}
