51b4d1562e6d7943a656ab655794b5a6e7e83ff4
Steve Bohlen <sbohlen@gmail.com>
2016-10-04 07:43:20 -0400

    strip unnecessary using statements from source (conflict w/ .NET 2.0 compilation anyway)

--- src/Common.Logging.Portable/Logging/Configuration/LogConfiguration.cs
+++ src/Common.Logging.Portable/Logging/Configuration/LogConfiguration.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Common.Logging.Configuration
+﻿namespace Common.Logging.Configuration
 {
     /// <summary>
     /// JSON serializable object representing the configuration of the logging subsystem.
