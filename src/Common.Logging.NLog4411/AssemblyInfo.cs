951c3e18fb72645fd087397a3b18b49d63418b0d
Steve Bohlen <sbohlen@gmail.com>
2017-07-16 12:38:31 -0400

    introducing support for NLog 4.4.11

---
+++ src/Common.Logging.NLog4411/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Security;
+
+[assembly: AssemblyProduct("Common Logging Framework NLog 4.4.11 Adapter")]
+[assembly: SecurityTransparent]
+
+#if NET_4_0
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+#endif
